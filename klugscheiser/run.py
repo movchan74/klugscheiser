5d390bdcd1ea78c2e076e1a84a724501d6b99869
Aleksandr Movchan <movchan174@gmail.com>
2025-03-02 19:39:59 +0100

    Added live translation.

--- klugscheiser/run.py
+++ klugscheiser/run.py
@@ -6,7 +6,7 @@ import logging
 import os
 import queue
 import threading
-from time import sleep
+import time
 
 import pyttsx3
 from deepgram import (
@@ -21,6 +21,17 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 load_dotenv()
+
+task = "translation"
+# language = "de"
+# language = "zh"
+language = "ru"
+# task = "klugscheiser"
+
+# Configure logger with timestamp
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 # We will collect the is_final=true messages here so we can use them when the person finishes speaking
 is_finals = []
@@ -53,26 +64,56 @@ def play_audio(text):
     audio_queue.put(text)
 
 
-# def is_question(text):
-#         # First, determine if this is a question
-#     check_response = openai_client.chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         messages=[
-#             # {
-#             #     "role": "system",
-#             #     "content": "Determine if the following text is a question. Respond with only 'yes' or 'no'.",
-#             # },
-#             {
-#                 "role": "user",
-#                 "content": f"Determine if the following text is a question: {text}. Respond with only 'yes' or 'no'.",
-#             },
-#         ],
-#         max_tokens=10,
-#     )
-#     response = check_response.choices[0].message.content.strip().lower()
-#     # remove any punctuation
-#     response = "".join(e for e in response if e.isalnum())
-#     return response.lower() == "yes"
+def process_translation(text):
+    try:
+        logging.info("Received Chinese text: %s", text)
+        start_time = time.time()
+        translation_response = openai_client.chat.completions.create(
+            # model="gpt-3.5-turbo",
+            # model="gpt-4o-mini",
+            model="gpt-4o",
+            messages=[
+                {
+                    "role": "system",
+                    "content": (
+                        "You are a translation assistant. Translate the following text to English. "
+                        # "You are a translation assistant. Translate the following Chinese text to English. "
+                        "The transcription might not be perfect, try to understand the meaning."
+                        "Respond with the translated text only."
+                    ),
+                },
+                {"role": "user", "content": text},
+            ],
+            max_tokens=150,
+        )
+        translation = translation_response.choices[0].message.content.strip()
+        logging.info("Translation Time: %s", time.time() - start_time)
+        logging.info("Translation: %s", translation)
+        play_audio(translation)
+    except Exception as e:
+        logging.error("Error translating text: %s", e)
+
+
+def answer_question(context, question):
+    answer_response = openai_client.chat.completions.create(
+        # model="gpt-3.5-turbo",
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": (
+                    "You are a helpful assistant providing concise answers. Keep your answers very short. "
+                    "For example, if the question is 'What is the capital of the United States?', "
+                    "only answer 'Washington, D.C.'. Use the context to understand the question."
+                ),
+            },
+            {"role": "user", "content": f"Context: {context}\nQuestion: {question}"},
+        ],
+        max_tokens=150,
+    )
+
+    answer = answer_response.choices[0].message.content.strip()
+    return answer
 
 
 def is_question(text):
@@ -81,47 +122,21 @@ def is_question(text):
     return False
 
 
-def is_question_and_answer(text):
-    """
-    Check if the text is a question and return an answer if it is.
-    """
+def process_question(text):
+    global previous_text
     try:
         _is_question = is_question(text)
         print(f"Is question: {_is_question}", "Text:", text)
 
         if _is_question:
-            # If it's a question, get an answer
-            answer_response = openai_client.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {
-                        "role": "system",
-                        "content": (
-                            "You are a helpful assistant providing concise answers. Keep your answers very short. "
-                            "For example, if the question is 'What is the capital of the United States?', "
-                            "only answer 'Washington, D.C.'"
-                        ),
-                    },
-                    {"role": "user", "content": text},
-                ],
-                max_tokens=150,
-            )
-
-            answer = answer_response.choices[0].message.content.strip()
-            return True, answer
-
-        return False, None
-
+            answer = answer_question(previous_text, text)
+            print(f"Question detected! Answer: {answer}")
+            play_audio(answer)
+        else:
+            print("Not a question")
+            previous_text += text
     except Exception as e:
         print(f"Error checking/answering question: {e}")
-        return False, None
-
-
-def process_question(utterance):
-    is_question, answer = is_question_and_answer(utterance)
-    if is_question:
-        print(f"Question detected! Answer: {answer}")
-        play_audio(answer)
 
 
 def main():
@@ -131,69 +146,51 @@ def main():
         dg_connection = deepgram.listen.websocket.v("1")
 
         def on_open(self, open, **kwargs):
-            print("Connection Open")
+            logging.info("Connection Open")
 
         def on_message(self, result, **kwargs):
             global is_finals
+            # print(f"Message: {result.to_json()}")
             sentence = result.channel.alternatives[0].transcript
             if len(sentence) == 0:
                 return
             if result.is_final:
-                print(f"Message: {result.to_json()}")
-                # We need to collect these and concatenate them together when we get a speech_final=true
-                # See docs: https://developers.deepgram.com/docs/understand-endpointing-interim-results
-                is_finals.append(sentence)
-
-                # Speech Final means we have detected sufficient silence to consider this end of speech
-                # Speech final is the lowest latency result as it triggers as soon an the endpointing value has triggered
-                if result.speech_final:
-                    utterance = " ".join(is_finals)
-                    print(f"Speech Final: {utterance}")
-                    threading.Thread(target=process_question, args=(utterance,)).start()
-                    is_finals = []
-                else:
-                    # These are useful if you need real time captioning and update what the Interim Results produced
-                    print(f"Is Final: {sentence}")
+                # print(f"Message: {result.to_json()}")
+                logging.info("Speech Final: %s", sentence)
+                threading.Thread(target=process_translation, args=(sentence,)).start()
             else:
-                # These are useful if you need real time captioning of what is being spoken
-                print(f"Interim Results: {sentence}")
+                logging.info("Interim Results: %s", sentence)
 
         def on_metadata(self, metadata, **kwargs):
-            print(f"Metadata: {metadata}")
+            logging.info("Metadata: %s", metadata)
 
         def on_speech_started(self, speech_started, **kwargs):
-            print("Speech Started")
-
-        def on_utterance_end(self, utterance_end, **kwargs):
-            print("Utterance End")
-            global is_finals
-            if len(is_finals) > 0:
-                utterance = " ".join(is_finals)
-                print(f"Utterance End: {utterance}")
-                threading.Thread(target=process_question, args=(utterance,)).start()
-                is_finals = []
+            logging.info("Speech Started")
 
         def on_close(self, close, **kwargs):
-            print("Connection Closed")
+            logging.info("Connection Closed")
 
         def on_error(self, error, **kwargs):
-            print(f"Handled Error: {error}")
+            logging.error("Handled Error: %s", error)
 
         def on_unhandled(self, unhandled, **kwargs):
-            print(f"Unhandled Websocket Message: {unhandled}")
+            logging.warning("Unhandled Websocket Message: %s", unhandled)
 
         dg_connection.on(LiveTranscriptionEvents.Open, on_open)
         dg_connection.on(LiveTranscriptionEvents.Transcript, on_message)
         dg_connection.on(LiveTranscriptionEvents.Metadata, on_metadata)
         dg_connection.on(LiveTranscriptionEvents.SpeechStarted, on_speech_started)
-        dg_connection.on(LiveTranscriptionEvents.UtteranceEnd, on_utterance_end)
         dg_connection.on(LiveTranscriptionEvents.Close, on_close)
         dg_connection.on(LiveTranscriptionEvents.Error, on_error)
         dg_connection.on(LiveTranscriptionEvents.Unhandled, on_unhandled)
 
         options: LiveOptions = LiveOptions(
-            model="nova-3",
-            language="en-US",
+            # model="nova-3",
+            # model="nova-2",
+            # language="en-US",
+            # language="zh",
+            # language="ru",
+            # language="de",
             # Apply smart formatting to the output
             smart_format=True,
             # Raw audio format details
@@ -202,20 +199,30 @@ def main():
             sample_rate=16000,
             # To get UtteranceEnd, the following must be set:
             interim_results=True,
+            # interim_results=False,
             utterance_end_ms="1000",
+            # utterance_end_ms="500",
             vad_events=True,
             # Time in milliseconds of silence to wait for before finalizing speech
-            endpointing=300,
+            endpointing=100,
+            # endpointing=300,
         )
+
+        if task == "klugscheiser":
+            options.model = "nova-3"
+            options.language = "en-US"
+        elif task == "translation":
+            options.model = "nova-2"
+            options.language = language
 
         addons = {
             # Prevent waiting for additional numbers
             "no_delay": "true"
         }
 
-        print("\n\nPress Enter to stop recording...\n\n")
+        logging.info("\n\nPress Enter to stop recording...\n\n")
         if dg_connection.start(options, addons=addons) is False:
-            print("Failed to connect to Deepgram")
+            logging.error("Failed to connect to Deepgram")
             return
 
         # Open a microphone stream on the default input device
@@ -233,12 +240,10 @@ def main():
         # Indicate that we've finished
         dg_connection.finish()
 
-        print("Finished")
-        # sleep(30)  # wait 30 seconds to see if there is any additional socket activity
-        # print("Really done!")
+        logging.info("Finished")
 
     except Exception as e:
-        print(f"Could not open socket: {e}")
+        logging.error("Could not open socket: %s", e)
         return
 
 
